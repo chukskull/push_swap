8844b173713ec75981f91dda29e714bf02d02fc9
Saleh Nagat <snagat@e2r9p1.1337.ma>
2022-03-23 12:07:18 +0100

    adding tests

---
+++ randoms.py
@@ -0,0 +1,18 @@
+import random
+import sys
+
+ints = []
+
+len = int(sys.argv[1])
+
+i = 0
+
+while i < len:
+	n = random.randint(0, 1000)
+	if n not in ints:
+		ints.append(n)
+		i += 1
+
+string_ints = [str(int) for int in ints]
+str_of_ints = " ".join(string_ints)
+print(str_of_ints)
---
+++ test.sh
@@ -0,0 +1,1 @@
+echo "Numbers : $(python3 randoms.py $@ > test && cat test) - \n\n\n $(./push_swap $(cat test) | wc -l) Lines, and Checker is : $(./push_swap $(cat test) | ./Checker $(cat test))"
